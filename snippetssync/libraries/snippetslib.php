71a5c2b1f033886b1730371db34c4ec3f381a9aa
Jannis Gundermann <jannis@gundermann.me>
2012-01-08 18:15:39 +1300

    Added automatic creation of snippets & global_variables folders if they don't exist. folder names can now be set via $['config'] items "snippetssync_sn_folder" and "snippetssync_gv_folder".

--- snippetssync/libraries/snippetslib.php
+++ snippetssync/libraries/snippetslib.php
@@ -13,101 +13,143 @@ require_once PATH_THIRD.'libraries/ab/ab_libbase'.EXT;
  
 class Snippetslib extends Ab_LibBase {
 
-    public $error_message = '';
-    public $last_sync_log = array();
+	public $error_message = '';
+	public $last_sync_log = array();
+	
+	private $sn_path = '';
+	private $gv_path = '';
+	private $tmpl_basepath = '';
 
-    public function verify_settings()
-    {
-        if($this->EE->config->item('save_tmpl_files') != 'y')
-        {
-            $this->error_message = 'Save templates as files must be set to Yes in Global Template Preferences';
-            return FALSE;
-        }
-        else
-        {
-            $tmpl_basepath = $this->EE->config->slash_item('tmpl_file_basepath') . $this->EE->config->slash_item('site_short_name');
-            if(!($tmpl_basepath != $this->EE->config->slash_item('site_short_name') && file_exists($tmpl_basepath)))
-            {
-                $this->error_message = 'Template basepath not defined - or not found ('.$tmpl_basepath.')';
-                return FALSE;
-            }
-        }
+	public function __construct() {
+		
+		parent::__construct();
+		
+		// setup the pathing to all required directories.
+		$this->tmpl_basepath = $this->EE->config->slash_item('tmpl_file_basepath') . $this->EE->config->slash_item('site_short_name');
+		$this->sn_path = $this->tmpl_basepath . ( $this->EE->config->slash_item('snippetssync_sn_folder') ? $this->EE->config->slash_item('snippetssync_sn_folder') : "snippets/" );
+		$this->gv_path = $this->tmpl_basepath . ( $this->EE->config->slash_item('snippetssync_gv_folder') ? $this->EE->config->slash_item('snippetssync_gv_folder') : "global_variables/" );
+	}
 
-        return TRUE;    // everything OK
-    }
+	public function verify_settings()
+	{
+		// checks if templates are saved as files.
+		if($this->EE->config->item('save_tmpl_files') != 'y')
+		{
+			$this->error_message = 'Save templates as files must be set to Yes in Global Template Preferences';
+			return FALSE;
+		}
+		else
+		{
+			if(!($this->tmpl_basepath != $this->EE->config->slash_item('site_short_name') && file_exists($this->tmpl_basepath)))
+			{
+				$this->error_message = 'Template basepath not defined - or not found ('.$this->tmpl_basepath.')';
+				return FALSE;
+			}
+		}
+		
+		
+		// check that parent dir is writeable.
+		if ( substr(sprintf('%o', fileperms( $this->tmpl_basepath )) , -4) < DIR_WRITE_MODE )
+		{
+			show_error( "Your template directory (".$this->tmpl_basepath.") needs to be writable." );
+		}
+		
+		// check if the global_vars and snippets dirs exists, else create them now.
+		// check global vars dir
+		if ( !file_exists($this->gv_path) )
+		{
+			mkdir( $this->gv_path );
+		}
+		if ( !$this->mk_perms( $this->gv_path ) )
+		{
+			show_error( "Could not make the global variables directory writeable." );
+		}
+		
+		// check snippets dir
+		if ( !file_exists($this->sn_path) )
+		{
+			mkdir( $this->sn_path );
+		}
+		if ( !$this->mk_perms( $this->sn_path ) )
+		{
+			show_error( "Could not make the snippets directory writeable." );
+		}
+		
+		return TRUE;	// everything OK
+	}
 
-    public function sync_all()
-    {
-        $this->last_sync_log['globals'] = array();
-        $this->last_sync_log['snippets'] = array();
+	public function sync_all()
+	{
+		$this->last_sync_log['globals'] = array();
+		$this->last_sync_log['snippets'] = array();
 
-        if($this->verify_settings())
-        {
-			$tmpl_basepath = $this->EE->config->slash_item('tmpl_file_basepath') . $this->EE->config->slash_item('site_short_name');
+		if($this->verify_settings())
+		{
 
-            $global_variables = $this->get_files($tmpl_basepath.'global_variables/');
-            $snippets = $this->get_files($tmpl_basepath.'snippets/');
+			$global_variables = $this->get_files($this->gv_path);
+			$snippets = $this->get_files($this->sn_path);
 
-            foreach($global_variables as $global_variable_filename)
-            {
-                $global_variable_name = str_replace('.html', '', $global_variable_filename);
+			foreach($global_variables as $global_variable_filename)
+			{
+				$global_variable_name = str_replace('.html', '', $global_variable_filename);
 
-                $this->EE->db->where('variable_name', $global_variable_name);
-                $this->EE->db->from('global_variables');
-                $global_variable_data = file_get_contents($tmpl_basepath.'global_variables/'.$global_variable_filename);
+				$this->EE->db->where('variable_name', $global_variable_name);
+				$this->EE->db->from('global_variables');
+				
+				$global_variable_data = file_get_contents($this->gv_path.$global_variable_filename);
 
-                if($this->EE->db->count_all_results() == 0)
-                {
-                    $this->EE->db->insert('global_variables', array(
-                        'variable_name' => $global_variable_name,
-                        'variable_data' => $global_variable_data,
-                    ));
-                }
-                else
-                {
-                    $this->EE->db->where('variable_name', $global_variable_name);
-                    $this->EE->db->update('global_variables', array('variable_data' => $global_variable_data));
-                }
+				if($this->EE->db->count_all_results() == 0)
+				{
+					$this->EE->db->insert('global_variables', array(
+						'variable_name' => $global_variable_name,
+						'variable_data' => $global_variable_data,
+					));
+				}
+				else
+				{
+					$this->EE->db->where('variable_name', $global_variable_name);
+					$this->EE->db->update('global_variables', array('variable_data' => $global_variable_data));
+				}
 
-                $this->last_sync_log['globals'][] = $global_variable_name;
-            }
+				$this->last_sync_log['globals'][] = $global_variable_name;
+			}
 
-            foreach($snippets as $snippet_filename)
-            {
-                $snippet_name = str_replace('.html', '', $snippet_filename);
+			foreach($snippets as $snippet_filename)
+			{
+				$snippet_name = str_replace('.html', '', $snippet_filename);
 
-                $this->EE->db->like('snippet_name', $snippet_name);
-                $this->EE->db->from('snippets');
-                $snippet_data = file_get_contents($tmpl_basepath.'snippets/'.$snippet_filename);
+				$this->EE->db->where('snippet_name', $snippet_name);
+				$this->EE->db->from('snippets');
+				$snippet_data = file_get_contents($this->sn_path.$snippet_filename);
 
-                if($this->EE->db->count_all_results() == 0)
-                {
-                    $this->EE->db->insert('snippets', array(
-                        'snippet_name' => $snippet_name,
-                        'snippet_contents' => $snippet_data,
-                    ));
+				if($this->EE->db->count_all_results() == 0)
+				{
+					$this->EE->db->insert('snippets', array(
+						'snippet_name' => $snippet_name,
+						'snippet_contents' => $snippet_data,
+					));
 
-                }
-                else
-                {
-                    $this->EE->db->where('snippet_name', $snippet_name);
-                    $this->EE->db->update('snippets', array('snippet_contents' => $snippet_data));
-                }
+				}
+				else
+				{
+					$this->EE->db->where('snippet_name', $snippet_name);
+					$this->EE->db->update('snippets', array('snippet_contents' => $snippet_data));
+				}
 
-                $this->last_sync_log['snippets'][] = $snippet_name;
-            }
+				$this->last_sync_log['snippets'][] = $snippet_name;
+			}
+			
+			return TRUE;
 
-            return TRUE;
-
-        }
-        else
-        {
-            return FALSE;
-        }
-    }
+		}
+		else
+		{
+			return FALSE;
+		}
+	}
 
 
-    /**
+	/**
 	 * Get dirs/files from a source_dir
 	 *
 	 * @param $source_dir
@@ -136,5 +178,26 @@ class Snippetslib extends Ab_LibBase {
 		return $files;
 	}
 
-
+	/**
+	 * Check permissions of dir/file for desired permissions.
+	 * If perms differ then set dir/file to desired perms.
+	 * 
+	 * @param $dir
+	 * @param $perms (default: 0777)
+	 */
+	private function mk_perms( $dir , $perms = DIR_WRITE_MODE )
+	{
+		clearstatcache();
+		
+		if ( substr(sprintf('%o', fileperms( $dir )), -4) != $perms )
+		{
+			chmod( $dir , $perms );
+		}
+		else {
+			return FALSE;
+		}
+		
+		return TRUE;
+	}
+	
 }
