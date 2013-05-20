47c387d6a7cb15b6ff2550132ce902ad7cd35a4d
Michael C. <michael.cohen@pro-image.co.il>
2013-05-20 16:26:19 +0300

    Added DevDemon Updater compatibility
    Modified add-on folder structure, placing "libraries" and "snippetssync"
    folders inside "/system/expressionengine/third_party/" folders. Updated
    "INSTALLING.txt" and "changelog.txt" accordingly.

---
+++ system/expressionengine/third_party/libraries/ab/ab_libbase.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'ab_common'.EXT;
+
+/**
+ * AddonBakery - Base for EE libraries; all libraries extend this
+ *
+ * All your base are belongs to us
+ */
+ 
+class Ab_LibBase extends Ab_Common {}
